032b4607257fabefafb101651afbf27f7f71cc5e
Andrés Pinto <andres.pinto.n@gmail.com>
2023-08-02 02:00:34 -0500

    exxcel

---
+++ src/api/proposal-card/controllers/proposal-card.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * proposal-card controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::proposal-card.proposal-card');
